340a47fd5264c51e137448db77e589f474d2d8d8
Guilherme Moura <mouraggui@gmail.com>
2022-09-11 14:51:39 -0300

    project startup

---
+++ README.md
@@ -0,0 +1,47 @@
+<p align="center">
+  <img style="object: contain; height: 150px" alt="RapidAPI" src="https://raw.githubusercontent.com/glhrmoura/imports-sorter/main/src/images/icon.png">
+</p>
+
+# :package: Imports Sorter
+
+Extension for Visual Studio Code that helps organize imports by sorting them based on their source folder.
+
+## Classification rules
+
+The extension uses a block-based classification system. In order, the blocks are:
+
+### Core
+
+Block dedicated to the core libraries of the project.
+
+**Examples:** react, vue, react native.
+
+### Libraries
+
+Block dedicated to third-party libraries.
+
+**Examples:** redux, vuex, enzyme, styled-components.
+
+### General
+
+Block dedicated to the general (non-component) imports of the project.
+
+**Examples:** services, constants, helpers.
+
+### Global components
+
+Block dedicated to imported global components, which are acquired from the project root.
+
+**Examples:** ~/components, @/components.
+
+### Local components
+
+Block dedicated to imported local components, which are acquired from a folder at the same level as the requesting file.
+
+**Example:** ./components.
+
+## Overview
+
+<p align="center">
+  <img src="https://github.com/glhrmoura/imports-sorter/raw/main/src/docs/overview.gif" />
+</p>
---
+++ src/index.js
@@ -0,0 +1,103 @@
+const vscode = require('vscode');
+
+const activate = (context) => {
+	const command = vscode.commands.registerCommand('importsSorter.sort', () => {
+		const editor = vscode.window.activeTextEditor;
+
+		if (!editor) return;
+
+		const selectedText = editor.document.getText(editor.selection);
+		const lines = selectedText.split(/;|'$/);
+
+		const coreBlock = [];
+		const libsBlock = [];
+		const generalGroup = [];
+		const globalComponentsBlock = [];
+		const localComponentsBlock = [];
+
+		const LINE_TYPES = {
+			Core: '1',
+			Libs: '2',
+			GlobalComponents: '3',
+			LocalComponents: '4',
+			General: '5',
+		};
+
+		const getLineType = (line) => {
+			const typesMap = {
+				[LINE_TYPES.Core]: /(from|import) ('react'|'react-native'|'vue')/g,
+				[LINE_TYPES.Libs]: /(from|import) '\w/g,
+				[LINE_TYPES.GlobalComponents]: /(from|import) '~\/components/ig,
+				[LINE_TYPES.LocalComponents]: /(from|import) '.\/components/ig,
+				[LINE_TYPES.General]: /(from|import) '(~|.|@)\//g,
+			};
+
+			return (Object.entries(typesMap)
+				.find(([, value]) => line.match(value)) || [])[0];
+		};
+
+		const getBlockString = (block) => {
+			return block
+				.map((line) => /'(\n|$)/.test(line) ? `${line};` : `${line}';`)
+				.map((line) => `${line.replace(/^[\r\n]+|[\r\n]+$/g, '')}\n`)
+				.sort((a, b) => a.length - b.length)
+				.join('');
+		};
+
+		lines.forEach((line) => {
+			switch (getLineType(line)) {
+				case LINE_TYPES.Core:
+					coreBlock.push(line);
+					break;
+				case LINE_TYPES.Libs:
+					libsBlock.push(line);
+					break;
+				case LINE_TYPES.GlobalComponents:
+					globalComponentsBlock.push(line);
+					break;
+				case LINE_TYPES.LocalComponents:
+					localComponentsBlock.push(line);
+					break;
+				case LINE_TYPES.General:
+					generalGroup.push(line);
+					break;
+				default:
+					break;
+			}
+		});
+		
+		const categorizedGeneral = generalGroup.reduce((groups, line) => {
+			const groupName = line.match(/(@|~|\.)\/([\w-]+)/)[2];
+
+			if (groups[groupName]) groups[groupName].push(line);
+			else groups[groupName] = [line];
+
+			return groups;
+		}, {});
+
+		const generalBlocks = Object.entries(categorizedGeneral)
+			.sort(([keyA], [keyB]) => keyA > keyB ? 1 : -1)
+			.map(([, block]) => block)
+
+		const importBlocks = [
+			getBlockString(coreBlock),
+			getBlockString(libsBlock),
+			...generalBlocks.map(getBlockString),
+			getBlockString(globalComponentsBlock),
+			getBlockString(localComponentsBlock),
+		].filter(Boolean).join('\n').replace(/\n+$/, '');
+
+		editor.edit(editBuilder => {
+			editBuilder.replace(editor.selection, importBlocks);
+		});
+	});
+
+	context.subscriptions.push(command);
+};
+
+const deactivate = () => {};
+
+module.exports = {
+	activate,
+	deactivate,
+};
